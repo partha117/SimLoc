1785c7b7656d3b3066ed682a19a946266e26a2d4
partha117 <1305117.pc@ugrad.cse.buet.ac.bd>
2023-12-16 17:40:08 -0500

    config files

--- simloc/report/finetune/DuplicateDetection.py
+++ simloc/report/finetune/DuplicateDetection.py
@@ -2,87 +2,109 @@ import torch
 import wandb
 import sys
 from pathlib import Path
+
+from sentence_transformers import SentenceTransformer, losses, InputExample, models
+
+from report.pretrain.CustomTransformer import CustomTransformer
+
 sys.path.append(str(Path(__file__).resolve().parents[1]))
-from torch.nn import BCELoss
+import logging
+from torch.nn import BCELoss, CosineEmbeddingLoss
 from torch.utils.data import DataLoader
 from transformers import AutoModel, AutoTokenizer
-from datasets import Dataset
+from datasets import Dataset, DatasetDict
 import os
 from torch import nn
 from torch.optim import Adam, AdamW
 from torch.optim.lr_scheduler import ReduceLROnPlateau, LinearLR
 from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, roc_auc_score
-from report.finetune.Models import DuplicateDetector
+from report.finetune.Models import DuplicateDetector, Residual
 from report.pretrain.TSDAE_Training import create_arg_parser
 from utils.Configuration import Configuration
-from utils.Helper import get_metrics, setup_logger
+from utils.Helper import get_metrics, setup_logger, get_pooler_class
+import shutil
+
+
+class LoggingCosineSimilarityLoss(losses.CosineSimilarityLoss):
+    def forward(self, sentence_features, labels):
+        loss_value = super().forward(sentence_features, labels)
+        logging.info(f'Loss: {loss_value.item()}')
+        run.log({"Loss": loss_value.item()})
+        return loss_value
 
 
 class FineTuneForDuplicateDetection:
     def __init__(self, config, save_path):
         self.config = config
-        self.device = config.device
-        self.model = AutoModel.from_pretrained(config.model_name_or_path)
-        self.tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path)
-        self.detector_model = DuplicateDetector(embedding_model=self.model,
-                                                embedding_dimension=config.embedding_dimension,
-                                                output_dimension=config.output_dimension,
-                                                residual_layers_count=0).to(self.device)
-        self.activator = nn.Sigmoid()
-        self.optimizer = eval(config.optimizer)(self.model.parameters(), lr=config.learning_rate)
-        self.scheduler = eval(config.scheduler)(self.optimizer, factor=config.weight_decay)
-        self.criterion = eval(config.criterion)
-        self.save_path = os.path.join(save_path, config.id)
-        self.metrics = [get_metrics(item) for item in config.metrics]
+        self.device = self.config.device
+        if not self.config.pool:
+            self.detector_model = SentenceTransformer(self.config.model_name_or_path, device=config.device)
+        else:
+            word_embedding_model = CustomTransformer(without_weights=False,
+                                                     model_name_or_path=config.model_name_or_path)
+            pooling_model = get_pooler_class(config.pool,
+                                             word_embedding_dimension=word_embedding_model.get_word_embedding_dimension())
+            modules = [word_embedding_model, pooling_model]
+            if self. config.residual_layer > 0:
+                self.residual_layers = nn.Sequential(
+                    *[Residual(in_features=config.embedding_dimension, out_features=config.output_dimension) for _ in
+                      range(self.residual_layers)])
+                modules.append(self.residual_layers)
+            self.detector_model = SentenceTransformer(modules=modules, device=config.device)
+        self.save_path = os.path.join(save_path, self.config.id)
+        self.metrics = [get_metrics(item) for item in self.config.metrics]
 
-    def train_model(self, data_path, config):
-        train_dataset = Dataset.load_from_disk(os.path.join(data_path, config.dataset))
-        train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=2)
-        for epoch in range(config.epochs):
-            for batch in train_loader:
-                input_ids1, attention_mask1 = self.tokenizer(batch['bug1'])
-                input_ids2, attention_mask2 = self.tokenizer(batch['bug2'])
-                label = batch['label']
-                prediction = self.detector_model(input_ids1.to(self.device), input_ids2.to(self.device), attention_mask1.to(self.device), attention_mask2.to(self.device))
-                loss = self.criterion(self.activator(prediction), label)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                self.scheduler.step()
+    def train_model(self, data_path):
+        train_dataset = DatasetDict.load_from_disk(os.path.join(data_path, self.config.dataset))['train']
+        train_dataset = train_dataset.map(lambda x: {"label": 1.0 if x['label'].lower() == 'yes' else 0.0, "texts": [x['bug1'], x['bug2']]})
+        train_examples = [InputExample(texts=item['texts'], label=item['label']) for item in train_dataset]
+        train_dataloader = DataLoader(train_examples, batch_size=self.config.batch_size, shuffle=True, num_workers=1)
+        train_loss = LoggingCosineSimilarityLoss(self.detector_model)
 
-    def evaluate_model(self, data_path, config):
-        test_dataset = Dataset.load_from_disk(os.path.join(data_path, config.dataset))
-        test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True, num_workers=2)
-        actual_value = []
-        predicted_value = []
-        for batch in test_loader:
-            input_ids1, attention_mask1 = self.tokenizer(batch['bug1'])
-            input_ids2, attention_mask2 = self.tokenizer(batch['bug2'])
-            label = batch['label']
-            with torch.no_grad():
-                prediction = self.activator(self.detector_model(input_ids1.to(self.device), input_ids2.to(self.device), attention_mask1.to(self.device), attention_mask2.to(self.device)))
-            actual_value.append(label)
-            predicted_value.extend(prediction.numpy().tolist())
-        self.write_results(actual_value, predicted_value)
+        # Tune the model
+        self.detector_model.fit(train_objectives=[(train_dataloader, train_loss)],
+                                epochs=self.config.epochs,
+                                scheduler=self.config.scheduler,#'warmuplinear',
+                                warmup_steps=self.config.warmup_steps,#500,
+                                optimizer_class=eval(self.config.optimizer),
+                                weight_decay=self.config.weight_decay,
+                                optimizer_params={'lr': self.config.learning_rate},
+                                checkpoint_save_total_limit=self.config.save_limit,
+                                checkpoint_save_steps=self.config.save_steps,
+                                checkpoint_path=f"Output/DuplicateDetection/checkpoints/" if save_path is None else save_path,
+                                show_progress_bar=True,
+                                use_amp=False)
+
+    def evaluate_model(self, data_path):
+        test_dataset = DatasetDict.load_from_disk(os.path.join(data_path, self.config.dataset))['test']
+        test_dataset = test_dataset.map(
+            lambda x: {"label": 1.0 if x['label'].lower() == 'yes' else 0.0, "texts": [x['bug1'], x['bug2']]})
+        similarity_method = nn.CosineSimilarity()
+        embedding1 = self.detector_model.encode(test_dataset['bug1'], batch_size=self.config.batch_size, convert_to_tensor=True, show_progress_bar=True, device=self.config.device)
+        embedding2 = self.detector_model.encode(test_dataset['bug2'], batch_size=self.config.batch_size,
+                                                convert_to_tensor=True, show_progress_bar=True, device=self.config.device)
+        predicted = similarity_method(embedding1, embedding2)
+        actual = test_dataset['label']
+        self.write_results(actual, predicted.cpu().numpy() > 0.8)
 
     def write_results(self, actual, predicted):
+        Path(self.save_path).mkdir(exist_ok=True, parents=True)
         with open(os.path.join(self.save_path, "results.txt"), "w") as f:
             for metric in self.metrics:
                 performance = metric(actual, predicted)
-                f.write(f"Metric: {metric} Performance: {performance}\n")
-                print(f"Metric: {metric} Performance: {performance}\n")
-        self.model.save_pretrained(os.path.join(self.save_path, "hf_model.pt"))
-        torch.save(self.detector_model.state_dict(), os.path.join(self.save_path, "model.pt"))
+                f.write(f"Metric: {metric.__name__} Performance: {performance}\n")
+                print(f"Metric: {metric.__name__} Performance: {performance}\n")
+
 
 
 if __name__ == "__main__":
-    # path = "../TrainingArgs/pretrain/2.json"
+    # path = "../../TrainingArgs/finetune/1.json"
     # config = Configuration(path=path)
-    # save_path = f"../../Output/{config.id}"
-    # data_path = "../../Data/Processed/"
+    # save_path = f"/home/p9chakra/MY_DRIVES/ProgramFiles/SimLoc/Output/{config.id}"
+    # data_path = "/home/p9chakra/MY_DRIVES/ProgramFiles/SimLoc/Data/Processed/"
     # dry_run = False
 
-    parser = create_arg_parser("TSDAE")
+    parser = create_arg_parser("DuplicateDetection")
     args = parser.parse_args()
     path = args.config_path
     config = Configuration(path=path)
@@ -101,5 +123,3 @@ if __name__ == "__main__":
     trainer = FineTuneForDuplicateDetection(config=config, save_path=save_path)
     trainer.train_model(data_path=data_path)
     trainer.evaluate_model(data_path=data_path)
-
-
--- simloc/report/finetune/Models.py
+++ simloc/report/finetune/Models.py
@@ -1,4 +1,6 @@
 from torch import nn
+
+
 class Residual(nn.Module):
     def __init__(self, in_features, out_features, norm_layer=None):
         super().__init__()
@@ -17,6 +19,8 @@ class Residual(nn.Module):
         if self.norm:
             return self.norm(raw)
         return raw
+
+
 class Projection(nn.Module):
     def __init__(self, in_features, out_features, norm_layer=None):
         super().__init__()
@@ -26,20 +30,31 @@ class Projection(nn.Module):
     def forward(self, input):
         pre_act = self.dense(input)
         return self.act(pre_act)
+
+
 class DuplicateDetector(nn.Module):
-    def __init__(self, embedding_model, embedding_dimension, output_dimension, residual_layers_count, pooling_method):
+    def __init__(self, embedding_model, residual_layers_count,output_dimension, pooling_method=None, embedding_dimension=None):
         super(DuplicateDetector, self).__init__()
         self.embedding_model = embedding_model
         self.residual = residual_layers_count
-        self.similarity = nn.CosineSimilarity()
         self.pooling_method = pooling_method
         if self.residual > 0:
-            self.residual_layers = nn.Sequential([Residual(in_features=embedding_dimension, out_features=output_dimension) for _ in range(self.residual)])
+            self.residual_layers = nn.Sequential(
+                *[Residual(in_features=embedding_dimension, out_features=output_dimension) for _ in
+                  range(self.residual)])
+
+    def _get_pooled_embeddings(self, embedding, attention):
+        features = dict()
+        features['token_embeddings'] = embedding
+        features['attention_mask'] = attention
+        return self.pooling_method.forward(features)['sentence_embedding']
+
     def forward(self, input_id1, input_id2, attention_mask1, attention_mask2):
-        embeddings1 = self.pooling_method(self.embedding_model(input_id1, attention_mask1))
-        embeddings2 = self.pooling_method(self.embedding_model(input_id2, attention_mask2))
+        embeddings1 = self._get_pooled_embeddings(self.embedding_model(input_id1, attention_mask1).last_hidden_state,
+                                                  attention_mask1)
+        embeddings2 = self._get_pooled_embeddings(self.embedding_model(input_id2, attention_mask2).last_hidden_state,
+                                                  attention_mask2)
         if self.residual > 0:
             embeddings1 = self.residual_layers(embeddings1)
             embeddings2 = self.residual_layers(embeddings2)
-        similarity = self.similarity(embeddings1, embeddings2)
-        return similarity
+        return embeddings1, embeddings2
